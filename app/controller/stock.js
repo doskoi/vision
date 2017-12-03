c71b0e18bba66c8e1b47af06f5ce6cb0957c094b
Sheng <doskoi.panda@gmail.com>
2017-12-03 12:50:12 +0800

    made json return correctly

--- app/controller/stock.js
+++ app/controller/stock.js
@@ -5,12 +5,20 @@ const Controller = require('egg').Controller;
 
 class StockController extends Controller {
   async index() {
+    // const result = await this.ctx.model.Stock.find({'code': 300098}).sort({'datetime': -1});
     const result = await this.ctx.model.Stock.aggregate([
-        { $sort: { code : 1, datetime: -1 } },
-        { $group: { _id: "$code", doc: { $first: "$$ROOT" } } }
-      ]).allowDiskUse(true);
-    
-    this.ctx.body = result;
+      { $sort: { datetime: -1 } },
+      { $group: { _id: '$code', doc: { $first: '$$ROOT' } } },
+    ]).allowDiskUse(true);
+
+    let stocks = result.map(function(meta) {
+      let stock = {'code': meta.doc.code,
+                   'chg': (meta.doc.price / meta.doc.last_close - 1) * 100};
+    //   console.log(stock);
+      return stock;
+    });
+
+    this.ctx.body = stocks;
   }
 }
 
